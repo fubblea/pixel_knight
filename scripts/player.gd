3323d5d55ed5519d36c14fb51ad00f000c946966
fubblea <ajay.arthur@gmail.com>
2025-07-22 15:18:29 +0300

    Added input actions for jumping and movement; updated player script for improved control and animation handling.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-@export var SPEED: float = 100.0
-@export var JUMP_VELOCITY: float = -300.0
+@export var SPEED: float = 75.0
+@export var JUMP_VELOCITY: float = -275.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -12,20 +12,37 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta # Gets the gravity vector from the project settings.
 
+	if is_dying:
+		velocity.x = 0 # Stop horizontal movement
+		move_and_slide()
+		return
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get direction that can be -1, 0, 1.
+	var direction := Input.get_axis("move_left", "move_right")
+
+	# Update the sprite animation based on the direction.
+	if direction < 0:
+		animated_sprite_2d.flip_h = true
+	elif direction > 0:
+		animated_sprite_2d.flip_h = false
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if is_dying:
-		velocity.x = 0 # Stop horizontal movement
+	# Play animations
+	if is_on_floor() and not is_dying:
+		if direction != 0:
+			animated_sprite_2d.play("run")
+		elif direction == 0:
+			animated_sprite_2d.play("idle")
+	elif not is_on_floor() and not is_dying:
+		animated_sprite_2d.play("jump")
 
 	move_and_slide()
 
